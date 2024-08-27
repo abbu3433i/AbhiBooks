fd3a98d337ef538cd63a8c8d1c9bb417392d3eb2
Abhishek gour <133512497+abbu3433i@users.noreply.github.com>
2024-08-27 15:23:57 +0530

    Update Freebook.jsx

--- frontend/src/Components/Freebook.jsx
+++ frontend/src/Components/Freebook.jsx
@@ -81,6 +81,7 @@ function Freebook() {
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
+               initialSlide: 1,
             },
           },
         ],
