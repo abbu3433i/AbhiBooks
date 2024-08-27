2985d50b04881ffea2aba7f0e6ce853f5235b958
Abhishek gour <133512497+abbu3433i@users.noreply.github.com>
2024-08-27 11:48:09 +0530

    Update Signup.jsx

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
       email : data.email,
       password : data.password
     }
-   await axios.post("http://localhost:3000/user/signup",userInfo)
+   await axios.post("https://abhibooks-backend.onrender.com",userInfo)
     .then((res)=>{
       console.log(res.data);
       if (res.data) {
