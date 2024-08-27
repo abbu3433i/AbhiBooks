6b791168155180c7227da5a9d12fd36d368261da
Abhishek gour <133512497+abbu3433i@users.noreply.github.com>
2024-08-27 12:43:14 +0530

    Update Login.jsx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       email : data.email,
       password : data.password
     }
-   await axios.post("https://abhibooks-backend.onrender.com",userInfo)
+   await axios.post("https://abhibooks-backend.onrender.com/user/login",userInfo)
     .then((res)=>{
       console.log(res.data);
       if (res.data) {
